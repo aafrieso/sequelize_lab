1e23703043a8dd75a683764e5084430820ea9da2
Austin Friesorger <aafrieso@svsu.edu>
2023-02-16 22:03:50 -0500

    add test data

---
+++ seeders/20230217030203-dog.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    // Seed commands
+
+    const date = new Date()
+
+    await queryInterface.bulkInsert('Dogs', [{
+      name: 'Buddy',
+      breed: 'Bulldog',
+      age: 2, 
+      createdAt: date,
+      updatedAt: date,
+    }])
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    // Commands to revert seed
+    
+    await queryInterface.bulkDelete('Dogs', null, {})
+
+  }
+};
